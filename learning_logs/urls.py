2e05fe06bccb03a8e144aa7b09c3f5923e0ae4f7
tagir gilmutdinov <tagiroid@gmail.com>
2023-02-21 00:49:16 +0500

    Added topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     # topics page
     path('topics/', views.topics, name='topics'),
+    # page for one topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -13,3 +13,11 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    # shows the only topic and all theirs posts
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
