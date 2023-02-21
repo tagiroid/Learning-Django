fdec0a15680ec2458969056cac6154fab18ea3bb
tagir gilmutdinov <tagiroid@gmail.com>
2023-02-22 01:23:16 +0500

    Syntax

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     #  homepage
-    path('', views.index, name = 'index'),
+    path('', views.index, name='index'),
     # topics page
     path('topics/', views.topics, name='topics'),
     # page for one topic
