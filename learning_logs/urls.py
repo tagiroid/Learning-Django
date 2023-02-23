504f93f5118ea60621aae514cd088da067841f87
tagir gilmutdinov <tagiroid@gmail.com>
2023-02-24 00:12:55 +0500

    Editing added

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('new_topic/', views.new_topic, name='new_topic'),
     # new entry page
     path('new_entry/<int:topic_id>', views.new_entry, name='new_entry'),
+    # entries editing page
+    path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry'),
 ]
