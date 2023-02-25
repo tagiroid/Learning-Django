736d2f513c0f23760adf8f02a2dcb07d67a6c290
tagir gilmutdinov <tagiroid@gmail.com>
2023-02-25 16:28:11 +0500

    registration page added

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 """URLs for users paths"""
 
 from django.urls import path, include
+from . import views
 
 
 app_name = 'users'
 urlpatterns = [
     # URL for authorization by default
     path('', include('django.contrib.auth.urls')),
+    # registration page
+    path('register/', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    # registration new user
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        # working with filled form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # login and redirecting to homepage
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    # show empty or invalid form
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
